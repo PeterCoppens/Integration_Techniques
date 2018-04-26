a054fd07a48907de427c722ae207a9ccddafa20d
SimonDirckx <36510235+SimonDirckx@users.noreply.github.com>
2018-04-26 09:45:53 +0200

    Update INTEGRATOR.m

--- INTEGRATOR.m
+++ INTEGRATOR.m
@@ -1,5 +1,6 @@
 MCI(@(x)abs(x),1000)
-T0(@(x)abs(x),10)
+T0(@(x)abs(x),5)
+T_rec(@(x)abs(x),1,4)
 function Q=MCI(fun,N)
 t=rand(N,1);
 t=2*t-1;
@@ -13,4 +14,11 @@ T=.5*(fun(-1)+fun(1));
 t=fun(linspace(-1+h,1-h,N-1));
 T=T+sum(t);
 T=h*T;
+end
+function T=T_rec(fun,j,m)
+if (j==0)
+    T=T0(fun,m);
+else
+    T=(4^j*T_rec(fun,j-1,m+1)-T_rec(fun,j-1,m))/((4^j)-1);
+end
 end
